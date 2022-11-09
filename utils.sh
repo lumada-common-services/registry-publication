1541c86cdf2c1273bf028545a985d53ea394a09c
Vitor Cardoso <vitor.cardoso@hitachivantara.com>
2022-11-09 09:27:46 +0000

    build command 3

--- utils.sh
+++ utils.sh
@@ -165,7 +165,7 @@ function uploadFilesFromCommand() {
   for file in ${files}; do
     deploy-command=$(echo ${file} | yq e '.deploy-command')
   
-    jfrog deploy-command -f ${workspace} --build-name ${build_name} --build-number ${build_number}
+    jfrog ${deploy-command} -f ${workspace} --build-name ${build_name} --build-number ${build_number}
 
   done
 
