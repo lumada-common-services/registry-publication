8ad872491c4e3dcd43dd20d841b9ac6dbfa85957
Vitor Cardoso <vitor.cardoso@hitachivantara.com>
2022-11-22 09:13:49 +0000

    ssss

--- scripts/entrypoint.py
+++ scripts/entrypoint.py
@@ -8,6 +8,8 @@ from PublishRepoManager import ArtifactoryRepoManager
 with open(os.getenv('INPUT_ARTIFACTS_CONFIG_FILE'), 'r') as stream:
     artifactsYaml = yaml.safe_load(stream)
 
+os.environ['JFROG_CLI_BUILD_URL'] = str(os.getenv('INPUT_BUILD_URL'))
+
 data = {"version": os.getenv('INPUT_BUILD_VERSION'),
         "buildName": os.getenv('INPUT_BUILD_NAME'),
         "buildNumber": os.getenv('INPUT_BUILD_NUMBER'),
